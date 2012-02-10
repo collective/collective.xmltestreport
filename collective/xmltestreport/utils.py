9ca6c94666a16ccd88aee1d857abeb9765d2332a
Josip Delic <delijati@googlemail.com>
2012-02-10 15:25:39 +0100

    add: added monkey patch to get at least a ugly output

--- collective/xmltestreport/utils.py
+++ collective/xmltestreport/utils.py
@@ -41,26 +41,26 @@ def prettyXML(tree):
     indent(tree)
     return ElementTree.tostring(tree)
 
+#XXX Ugly monkey patch to get it at least working,
+# better ulgy output than no output
 
-#XXX ugly monkey patch
-def _escape_cdata(text, encoding):
-    # escape character data
+def patched_write_encoded(fname, text, encoding='utf8', errors='strict'):
+    '''Write utf8 string `text` to file names `fname`, with encoding.'''
+    import codecs
+    f = codecs.open(fname, 'w', encoding=encoding, errors=errors)
     try:
-        # it's worth avoiding do-nothing calls for strings that are
-        # shorter than 500 character, or so.  assume that's, by far,
-        # the most common case in most applications.
-        if "&" in text:
-            text = text.replace("&", "&amp;")
-        if "<" in text:
-            text = text.replace("<", "&lt;")
-        if ">" in text:
-            text = text.replace(">", "&gt;")
-        return text.encode(encoding, "xmlcharrefreplace")
-    except (TypeError, AttributeError):
-        ElementTree._raise_serialization_error(text)
-    #XXX this is the patch
-    except (UnicodeDecodeError):
-        return text.decode("utf-8").encode(encoding, "xmlcharrefreplace")
+        f.write(text.decode('utf8'))
+    except UnicodeDecodeError:
+        print "UnicodeDecodeError in file (%s)" % fname
+        f = codecs.open(fname, 'w', errors=errors)
+        f.write(text)
+    finally:
+        f.close()
 
-ElementTree._escape_cdata = _escape_cdata
+def write_html(self, fname, html):
+    """Write `html` to `fname`, properly encoded."""
+    patched_write_encoded(fname, html, 'ascii', 'xmlcharrefreplace') 
+
+import coverage.html
+coverage.html.HtmlReporter.write_html = write_html
 
